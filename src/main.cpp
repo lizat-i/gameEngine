debf9354c199255d4095220662174931991bb81a
ivan lizat <ivan.lizat@outlook.de>
2024-01-13 14:58:08 +0100

    add creator and builder logic to the system

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,22 @@
 int main()
 {
     EntityManager m_EntityManager;
-    // buildEntities(m_EntityManager,m_EntityManager);
-    try {
-        std::filesystem::path currentPath = std::filesystem::current_path();
-        std::cout << "Current working directory: " << currentPath << std::endl;
-    } catch (const std::filesystem::filesystem_error& e) {
-        std::cerr << "Error: " << e.what() << std::endl;
-    }
 
+    /*
+
+        add the reading and entity creating logic into a separate header or function
+        or mayb even class. 
+
+
+        // buildEntities(m_EntityManager,m_EntityManager);
+        try {
+            std::filesystem::path currentPath = std::filesystem::current_path();
+            std::cout << "Current working directory: " << currentPath << std::endl;
+        } catch (const std::filesystem::filesystem_error& e) {
+            std::cerr << "Error: " << e.what() << std::endl;
+        }
+
+    */
     auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
     window.setFramerateLimit(144);
 
